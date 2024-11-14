9b012313d4af010eb9e590b7257d882df0096cf9
Manuel Graziani <manuelgraziani42@gmail.com>
2024-11-14 01:07:25 +0100

    Added early list of pokemon

---
+++ src/components/Nav.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <nav class="navbar bg-danger">
+    <div class="container-fluid justify-content-center">
+      <RouterLink to="/" class="navbar-brand" href="#">
+        <img src="../assets/logo.webp" alt="Bootstrap" width="200" />
+      </RouterLink>
+    </div>
+  </nav>
+</template>
+
+<style scoped></style>
