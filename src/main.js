c909d4204bac4e096b56310fb33f7cc1c9f18cd2
Manuel Graziani <manuelgraziani42@gmail.com>
2024-11-15 17:08:30 +0100

    removed pinia dependencies

--- src/main.js
+++ src/main.js
@@ -3,14 +3,12 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/dist/js/bootstrap'
 import './assets/base.css'
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
